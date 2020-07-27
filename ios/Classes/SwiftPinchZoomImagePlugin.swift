fa99687a582d04eb52a8495f26f9c5ced072e5e4
Heshanka <heshanka123@gmail.com>
2020-07-27 17:38:43 +0300

    Update SwiftPinchZoomImagePlugin.swift

--- ios/Classes/SwiftPinchZoomImagePlugin.swift
+++ ios/Classes/SwiftPinchZoomImagePlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftPinchZoomImagePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "pos_pinch_zoom_image", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "pinch_zoom_image", binaryMessenger: registrar.messenger())
     let instance = SwiftPinchZoomImagePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
